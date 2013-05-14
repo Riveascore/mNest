6d7f59d070895292d1e5cef7d83f6918fae5e85e
Riveascore <agaxx010@morris.umn.edu>
2013-05-14 13:30:59 -0500

    Can now drag AND resize an element. Resizing dynamically resizes the child element inside of the div object. Changed child's value to absolute and parent's position value to relative to prevent a bug where the child would be outside the parent until you resized it.

--- mNest/grails-app/taglib/com/company/project/TwitchTagLib.groovy
+++ mNest/grails-app/taglib/com/company/project/TwitchTagLib.groovy
@@ -3,20 +3,22 @@ package com.company.project
 class TwitchTagLib {
 	def twitchStream = {attrs->
 		def videoKey = attrs['videoKey']
-		def height = "378", width = "620"
 		def vd = {
 			object(
 				type:"application/x-shockwave-flash", 
-				height:"378", width:"620", 
-				data:"http://www.twitch.tv/widgets/live_embed_player.swf?channel=${videoKey}&auto_play=true"){
+				class:"twitchVideoObject",
+				data:"http://www.twitch.tv/widgets/live_embed_player.swf?channel=${videoKey}&auto_play=true"
+				){
 				param(name:"allowFullScreen", value:"true")
 				param(name:"allowNetworking", value:"all")
 				param(name:"movie", value:"http://www.twitch.tv/widgets/live_embed_player.swf")
 			}
 		}
 		def xml = new groovy.xml.StreamingMarkupBuilder().bind(vd)
-		def divOpening = "<div class = 'furniture' id='twitch_stream_${videoKey}' height='${height}' width='${width}'>"
-		def moveable = "<script type='text/javascript'> \$('#twitch_stream_${videoKey}').show(); \$('#twitch_stream_${videoKey}').draggable() </script>"
-		out << "${divOpening}${xml}</div>${moveable}"
+		def divOpening = "<div class = 'twitchVideo' id='twitch_stream_${videoKey}'>"
+		def draggable = "\$('#twitch_stream_${videoKey}').draggable();"
+		def resizable = "\$('#twitch_stream_${videoKey}').resizable();"
+		def resizeablePerserveAspect = "\$('#twitch_stream_${videoKey}').resizable({aspectRatio: 622 / 380});"
+		out << "${divOpening}${xml}</div><script type='text/javascript'>${draggable}${resizeablePerserveAspect}</script>"
 	}
 }
--- mNest/grails-app/views/index.gsp
+++ mNest/grails-app/views/index.gsp
@@ -3,6 +3,7 @@
 	<head>
 		<meta name="layout" content="main"/>
 		<title>Welcome to Grails</title>
+		
 		<style type="text/css" media="screen">
 			#status {
 				background-color: #eee;
@@ -105,6 +106,8 @@
 		<div id="page-body" role="main">
 			<h1>Welcome to Grails</h1>
 			
+			<g:twitchStream videoKey="ocelote" />
+			
 			<p>Congratulations, you have successfully started your first Grails application! At the moment
 			   this is the default page, feel free to modify it to either redirect to a controller or display whatever
 			   content you may choose. Below is a list of controllers that are currently deployed in this application,
@@ -112,7 +115,6 @@
 
 			<div id="controller-list" role="navigation">
 				<h2>Available Controllers:</h2>
-				<g:twitchStream videoKey="ocelote" />
 				
 				<ul>
 					<g:each var="c" in="${grailsApplication.controllerClasses.sort { it.fullName } }">
